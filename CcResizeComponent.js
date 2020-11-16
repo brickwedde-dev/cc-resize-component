c5e84021a12c3171291ba260d6a270b21fb0cb2f
Alexander Brickwedde <alex_git@brickwedde.de>
2020-11-16 09:31:49 +0100

    Added raster support

--- CcResizeComponent.js
+++ CcResizeComponent.js
@@ -4,6 +4,8 @@ class CcResizeComponent extends HTMLElement {
     this.component = component;
     this.modal = modal;
     
+    this.raster = {x : 1, y: 1};
+
     this.mousemove = this.mousemove.bind(this);
     this.mouseup = this.mouseup.bind(this);
 
@@ -18,8 +20,8 @@ class CcResizeComponent extends HTMLElement {
     this.width = this.component.offsetWidth;
     this.height = this.component.offsetHeight;
 
-    this.componentOffsetTop = this.component.offsetTop;
-    this.componentOffsetLeft = this.component.offsetLeft;
+    this.componentOffsetTop = coord.top - this.component.offsetTop;
+    this.componentOffsetLeft = coord.left - this.component.offsetLeft;
 
     if (this.modal) {
       this.style.boxSizing = "border-box";
@@ -76,6 +78,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "m"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -95,6 +101,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "br"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -116,6 +126,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "tl"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -137,6 +151,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "tr"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -158,6 +176,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "bl"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -179,6 +201,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "t"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -200,6 +226,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "l"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -222,6 +252,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "b"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -242,6 +276,10 @@ class CcResizeComponent extends HTMLElement {
         return;
       }
       this.movestart = { x: e.clientX, y: e.clientY, pos: "r"};
+      this.origheight = this.height;
+      this.origwidth = this.width;
+      this.origtop = this.top;
+      this.origleft = this.left;
       e.preventDefault();
       e.stopPropagation();
     });
@@ -265,57 +303,49 @@ class CcResizeComponent extends HTMLElement {
     if (this.movestart) {
       switch (this.movestart.pos) {
         case "br":
-          this.height += e.clientY - this.movestart.y;
-          this.width += e.clientX - this.movestart.x;
+          this.height = rasterize(this.origheight + (e.clientY - this.movestart.y), this.raster.y, this.top - this.componentOffsetTop);
+          this.width = rasterize(this.origwidth + (e.clientX - this.movestart.x), this.raster.x, this.left - this.componentOffsetLeft);
           break;
         case "r":
-          this.width += e.clientX - this.movestart.x;
+          this.width = rasterize(this.origwidth + (e.clientX - this.movestart.x), this.raster.x, this.left - this.componentOffsetLeft);
           break;
         case "b":
-          this.height += e.clientY - this.movestart.y;
+          this.height = rasterize(this.origheight + (e.clientY - this.movestart.y), this.raster.y, this.top - this.componentOffsetTop);
           break;
         case "tl":
-          this.componentOffsetTop += e.clientY - this.movestart.y;
-          this.top += e.clientY - this.movestart.y;
-          this.componentOffsetLeft += e.clientX - this.movestart.x;
-          this.left += e.clientX - this.movestart.x;
-          this.height -= e.clientY - this.movestart.y;
-          this.width -= e.clientX - this.movestart.x;
+          this.top = rasterize(this.origtop + (e.clientY - this.movestart.y), this.raster.y, 0 - this.componentOffsetTop);
+          this.left = rasterize(this.origleft + (e.clientX - this.movestart.x), this.raster.x, 0 - this.componentOffsetLeft);
+          this.height = this.origheight - (this.top - this.origtop);
+          this.width = this.origwidth - (this.left - this.origleft);
           break;
         case "t":
-          this.componentOffsetTop += e.clientY - this.movestart.y;
-          this.top += e.clientY - this.movestart.y;
-          this.height -= e.clientY - this.movestart.y;
+          this.top = rasterize(this.origtop + (e.clientY - this.movestart.y), this.raster.y, 0 - this.componentOffsetTop);
+          this.height = this.origheight - (this.top - this.origtop);
           break;
         case "l":
-          this.componentOffsetLeft += e.clientX - this.movestart.x;
-          this.left += e.clientX - this.movestart.x;
-          this.width -= e.clientX - this.movestart.x;
+          this.left = rasterize(this.origleft + (e.clientX - this.movestart.x), this.raster.x, 0 - this.componentOffsetLeft);
+          this.width = this.origwidth - (this.left - this.origleft);
           break;
         case "tr":
-          this.componentOffsetTop += e.clientY - this.movestart.y;
-          this.top += e.clientY - this.movestart.y;
-          this.height -= e.clientY - this.movestart.y;
-          this.width += e.clientX - this.movestart.x;
+          this.top = rasterize(this.origtop + (e.clientY - this.movestart.y), this.raster.y, 0 - this.componentOffsetTop);
+          this.height = this.origheight - (this.top - this.origtop);
+          this.width = rasterize(this.origwidth + (e.clientX - this.movestart.x), this.raster.x, this.left - this.componentOffsetLeft);
           break;
         case "bl":
-          this.componentOffsetLeft += e.clientX - this.movestart.x;
-          this.left += e.clientX - this.movestart.x;
-          this.width -= e.clientX - this.movestart.x;
-          this.height += e.clientY - this.movestart.y;
+          this.left = rasterize(this.origleft + (e.clientX - this.movestart.x), this.raster.x, 0 - this.componentOffsetLeft);
+          this.width = this.origwidth - (this.left - this.origleft);
+          this.height = rasterize(this.origheight + (e.clientY - this.movestart.y), this.raster.y, this.top - this.componentOffsetTop);
           break;
         case "m":
-          this.componentOffsetTop += e.clientY - this.movestart.y;
-          this.top += e.clientY - this.movestart.y;
-          this.componentOffsetLeft += e.clientX - this.movestart.x;
-          this.left += e.clientX - this.movestart.x;
+          this.top = rasterize(this.origtop + (e.clientY - this.movestart.y), this.raster.y, 0 - this.componentOffsetTop);
+          this.left = rasterize(this.origleft + (e.clientX - this.movestart.x), this.raster.x, 0 - this.componentOffsetLeft);
           break;
       }
-      this.movestart.x = e.clientX;
-      this.movestart.y = e.clientY;
+      // this.movestart.x = e.clientX;
+      // this.movestart.y = e.clientY;
 
       this.resizeborder();
-      this.dispatchEvent(new CustomEvent("resized", {detail: {top: this.componentOffsetTop, left: this.componentOffsetLeft, width: this.width, height: this.height}}));
+      this.dispatchEvent(new CustomEvent("resized", {detail: {top: this.top - this.componentOffsetTop, left: this.left - this.componentOffsetLeft, width: this.width, height: this.height}}));
     }
   }
 
@@ -360,4 +390,18 @@ function getCoords(elem) { // crossbrowser version
   var left = box.left + scrollLeft - clientLeft;
 
   return { top: Math.round(top), left: Math.round(left) };
+}
+
+function rasterize (x, step, offset) {
+  if (step == 1) {
+    return x;
+  }
+  var mod = (x + offset) % step;
+  if (mod == 0) {
+    return x;
+  }
+  if (mod < step / 2) {
+    return x - mod;
+  }
+  return x + (step - mod);
 }
